7cd63c03283b390f7717c6a2924675c848b6b6df
Victor Ramirez <VRamirez75@gmail.com>
2019-07-26 15:09:20 -0700

    Display array of images return by the web API.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const request = require('request');
 const mysql = require('mysql');
 
 // creating a “route”
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
     /*
     console.log('in the app.get function');    
@@ -31,63 +31,69 @@ app.get("/", function(req, res){
     */
     var requestURL = "https://api.unsplash.com/photos/random?client_id=87deea8bf0db05f15dc94780a1b0353a53f4da628290e91042928a8bfded2050&orientation=landscape";
 
-    request(requestURL, function (error, response, body) 
-    {        
+    request(requestURL, function(error, response, body) {
         /*
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the API data
         */
-       if (!error){
-        var parseData = JSON.parse(body);    
-        // console.log("image url: ", parseData['urls']['regular']);    
-        var imageURL = parseData['urls']['regular'];    
-        // console.log("*****imageURL*****: ", imageURL);    
+        if (!error) {
+            var parseData = JSON.parse(body);
+            // console.log("image url: ", parseData['urls']['regular']);    
+            var imageURL = parseData['urls']['regular'];
+            // console.log("*****imageURL*****: ", imageURL);    
 
-        // res.render("index.html", {"imageURL": imageURL});
-        res.render("index", {"imageURL": imageURL});
-       } // if
-       else{
-           // res.render("index.html", {"error": "Unable to access API"});  
-           res.render("index", {"error": "Unable to access API"});  
+            // res.render("index.html", {"imageURL": imageURL});
+            res.render("index", {
+                "imageURL": imageURL
+            });
+        } // if
+        else {
+            // res.render("index.html", {"error": "Unable to access API"});  
+            res.render("index", {
+                "error": "Unable to access API"
+            });
         }
     }); // request
-    
+
 }); // root route
 
 // creating a “route”
-app.get("/search", function(req, res){
+app.get("/search", function(req, res) {
 
     // keyword enterd by user
     var keyword = req.query.keyword;
 
-    var requestURL = "https://api.unsplash.com/photos/random?query="+keyword+"&count=9&client_id=87deea8bf0db05f15dc94780a1b0353a53f4da628290e91042928a8bfded2050&orientation=landscape";
+    var requestURL = "https://api.unsplash.com/photos/random?query=" + keyword + "&count=9&client_id=87deea8bf0db05f15dc94780a1b0353a53f4da628290e91042928a8bfded2050&orientation=landscape";
 
-    request(requestURL, function (error, response, body) 
-    {        
-       if (!error){
-        var parseData = JSON.parse(body);    
-        
-        var imageURLs = [];
-        
-        for(let i = 0; i < 9; i++){
+    request(requestURL, function(error, response, body) {
+        if (!error) {
+            var parseData = JSON.parse(body);
 
-            imageURLs.push(parseData[i].urls.regular);    
-        } // for
+            var imageURLs = [];
 
-        console.log(imageURLs);
+            for (let i = 0; i < 9; i++) {
 
-        res.render("results", {"imageURLs": imageURLs});
+                imageURLs.push(parseData[i].urls.regular);
+            } // for
 
-       } // if
-       else{
-        
-            res.render("results", {"error": "Unable to access API"});  
-        
+            //console.log(imageURLs);
+
+            res.render("results", {
+                "imageURLs": imageURLs
+            });
+
+        } // if
+        else {
+
+            res.render("results", {
+                "error": "Unable to access API"
+            });
+
         } // else
 
     }); // request
-    
+
 }); // search route
 
 // allow the server to listen for any request
