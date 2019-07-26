a52bd4eec27d65201514be22f18cceee34922dd6
Victor Ramirez <VRamirez75@gmail.com>
2019-07-26 00:41:08 -0700

    added the web api call.

--- app.js
+++ app.js
@@ -18,30 +18,28 @@ app.get("/", function(req, res){
 
     console.log('in the app.get function');
     
-    console.log('calling the web API: var requestURL = "https://api.unsplash.com/photos/random?client_id=87deea8bf0db05f15dc94780a1b0353a53f4da628290e91042928a8bfded2050&orientation=landscape";');
+    console.log('settign the var to the web API: var requestURL = ', requestURL);
     var requestURL = "https://api.unsplash.com/photos/random?client_id=87deea8bf0db05f15dc94780a1b0353a53f4da628290e91042928a8bfded2050&orientation=landscape";
-    console.log('after calling the web API');
+    console.log('after setting the var to the web API');
 
-    //alert("requestURL", requestURL);
-
-/*    
     request(requestURL, function (error, response, body) 
     {
         
-        //console.log('error:', error); // Print the error if one occurred
-        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //console.log('body:', body); // Print the API data
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the API data
     
         var parseData = JSON.parse(body);    
-        var imageURL = parseData["urls"]["regular"];    
+        console.log("image url: ", parseData['urls']['regular']);    
+        var imageURL = parseData['urls']['regular'];    
 
         res.render("index", {"imageURL": imageURL});
     
     }); // request
     
         // res.render("index.html");
-  */
-    res.render("index.html");
+  
+    // res.render("index.html");
 });
 
 // allow the server to listen for any request
