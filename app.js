390f5853aa7955fefd8fab2cea5ac3f286010d03
Victor Ramirez <VRamirez75@gmail.com>
2019-07-25 23:30:05 -0700

    blow up the app.

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/**
- * Class: CST-336
- * Lab 5: Express, Embedded JavaScript (EJS), Request, and MySQl
- * Authors:
- * - Victor Ramirez
- **/
 
 // import Express library
 const express = require("express");
@@ -12,18 +6,83 @@ const express = require("express");
 const app = express();
 // render HTML
 app.engine('html', require('ejs').renderFile);
-
-app.use(express.static('public'));
-
-// import request library
-const request = require('request');
-// import mysql library
-const mysql = require('mysql');
+// add static files folder 
+app.use(express.static("public"));
 
 // creating a “route”
 app.get("/", function(req, res){
 
     res.render("index.html");
+
+});
+
+// creating a “route”
+app.get("/mercury", function(req, res){
+
+    res.render("mercury.html");
+
+});
+
+// creating a “route”
+app.get("/venus", function(req, res){
+
+    res.render("venus.html");
+
+});
+
+// creating a “route”
+app.get("/earth", function(req, res){
+
+    res.render("earth.html");
+
+});
+
+// creating a “route”
+app.get("/mars", function(req, res){
+
+    res.render("mars.html");
+
+});
+
+// creating a “route”
+app.get("/jupiter", function(req, res){
+
+    res.render("jupiter.html");
+
+});
+
+// creating a “route”
+app.get("/saturn", function(req, res){
+
+    res.render("saturn.html");
+
+});
+
+// creating a “route”
+app.get("/uranus", function(req, res){
+
+    res.render("uranus.html");
+
+});
+
+// creating a “route”
+app.get("/neptune", function(req, res){
+
+    res.render("neptune.html");
+
+});
+
+// creating a “route”
+app.get("/pluto", function(req, res){
+
+    res.render("pluto.html");
+
+});
+
+// creating a “route”
+app.get("/planet9", function(req, res){
+
+    res.render("planet9.html");
 
 });
 
