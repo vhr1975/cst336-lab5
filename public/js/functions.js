35ad82d48ebddd3697b897fa8ada17cfb29d5c20
Victor Ramirez <VRamirez75@gmail.com>
2019-07-26 21:55:55 -0700

    Toggle the fav icon.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,9 +1,16 @@
+/**
+ * Class: CST-336
+ * Lab 5: Express, Embedded JavaScript (EJS), Request, and MySQl
+ * Authors:
+ * - Victor Ramirez
+ **/
 
 $(document).ready(function(){
 
     $(".favoriteIcon").on("click", function(){
 
-        alert("it works!");
+        // alert("it works!");
+        $(this).attr("src", "img/fav_on.png");
 
     });
 
