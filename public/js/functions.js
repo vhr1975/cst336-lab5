f2fb7c3987072acc0ec1a4d10e8c63d41f74e236
Victor Ramirez <VRamirez75@gmail.com>
2019-07-26 22:17:23 -0700

    Testing code.

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,11 +5,15 @@
  * - Victor Ramirez
  **/
 
+ /*
+    function to toggle the fav icon on and off
+ */
 $(document).ready(function () {
 
 	$(".favoriteIcon").on("click", function () {
 
-		// alert("it works!");
+        alert($(this).attr("src"));
+        
 		if ($(this).attr("src") == "img/fav_off.png") {
 			$(this).attr("src", "img/fav_on.png");
 		} // if
