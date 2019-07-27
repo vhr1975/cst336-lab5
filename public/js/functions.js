40fc0e129547749fd08252304a61cc64449cae15
Victor Ramirez <VRamirez75@gmail.com>
2019-07-26 22:00:20 -0700

    Toggle the fav icon on and off.

--- public/js/functions.js
+++ public/js/functions.js
@@ -7,11 +7,19 @@
 
 $(document).ready(function(){
 
-    $(".favoriteIcon").on("click", function(){
+    $(".favoriteIcon").on("click", function()
+    {
 
         // alert("it works!");
-        $(this).attr("src", "img/fav_on.png");
+        if($(this).attr("src") == "img/fav_off.png") 
+        {
+            $(this).attr("src", "img/fav_on.png");            
+        }// if
+        else
+        {
+            $(this).attr("src", "img/fav_off.png");            
+        }// else        
 
-    });
+    }); // on click
 
-});
+});// doc ready
