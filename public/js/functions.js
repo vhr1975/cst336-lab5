85e581bc1b684a8fbf5324e2b548f24e2273b6c4
Victor Ramirez <VRamirez75@gmail.com>
2019-07-27 00:09:23 -0700

    Added AJax to save image url to the db.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,3 +1,5 @@
+import { get } from "https";
+
 /**
  * Class: CST-336
  * Lab 5: Express, Embedded JavaScript (EJS), Request, and MySQl
@@ -12,15 +14,29 @@ $(document).ready(function () {
 
 	$(".favoriteIcon").on("click", function () {
 
-        alert($(this).prev().attr("src"));
+        // alert($(this).prev().attr("src"));
+
+        var imageURL = $(this).prev().attr("src");
         
 		if ($(this).attr("src") == "img/fav_off.png") {
-			$(this).attr("src", "img/fav_on.png");
+            $(this).attr("src", "img/fav_on.png");
+            updateFavorite(imageURL);            
 		} // if
 		else {
-			$(this).attr("src", "img/fav_off.png");
+            $(this).attr("src", "img/fav_off.png");
+            
 		} // else        
 
-	}); // on click
+    }); // on click
+    
+    function updateFavorite (imageURL){
+
+        $.ajax({
+            method: "get",
+            url: "/api/updateFavorites",
+            data: {"imageURL" : imageURL, "keyword" : "coming soon!"}
+        }); // ajax
+        
+    } // function
 
 }); // doc ready
